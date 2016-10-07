16fb9bddfc1cf9911cb6606a7d44762da07240f3
Basanta Thapa <mabimal@basantathapa.com.np>
2016-10-07 11:56:19 -0400

    First Commit

---
+++ src/main/java/com/mabimal/MabimalTryApplication.java
@@ -0,0 +1,24 @@
+package com.mabimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@Controller
+public class MabimalTryApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabimalTryApplication.class, args);
+    }
+}
